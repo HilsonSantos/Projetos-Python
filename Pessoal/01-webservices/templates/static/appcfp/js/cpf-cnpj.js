c4395afe15fb1a8ffc3695b4d6bb9431fa16562e
Hilson Santos <94616249+HilsonSantos@users.noreply.github.com>
2024-02-14 21:45:42 -0300

    Atualização em 14022024-21:45:35

--- Pessoal/01-webservices/templates/static/appcfp/js/cpf-cnpj.js
+++ Pessoal/01-webservices/templates/static/appcfp/js/cpf-cnpj.js
@@ -1,86 +1,100 @@
 function validarCpfCnpj(){
-    /* A variável recebe o valor digitado no formulário */
+    /* A variável recebe o número do CPF ou CNPJ digitado no formulário */
     let numInformado = document.getElementById('cpf_cnpj').value
+    /* A variável recebe o tipo "F" ou "J" digitado no formulário */
     let tipInformado = document.getElementById('tipo').value
     let numValidar = numInformado
+    let numQuantidade = numValidar.length
     let soma = 0
 
-    /* */
-    if (numValidar.length == 11){
+    /* Valida o CPF */
+    if (numQuantidade == 11){
         let cpf = numValidar
         /* Calcula os dígitos verificadores */
         for (let i = 0; i < 9; i++) {
             dig = cpf[i]
-            soma += parseInt(dig) * (10 - i);
+            soma += parseInt(dig) * (10 - i)
         }
 
-        let resto = soma % 11;
-        let digito1 = resto < 2 ? 0 : 11 - resto;
+        let resto = soma % 11
+        let digito1 = resto < 2 ? 0 : 11 - resto
 
         soma = 0
         for (let i = 0; i < 10; i++) {
             dig = cpf[i]
-            soma += parseInt(dig) * (11 - i);
+            soma += parseInt(dig) * (11 - i)
         }
 
-        resto = soma % 11;
-        let digito2 = resto < 2 ? 0 : 11 - resto;
+        resto = soma % 11
+        let digito2 = resto < 2 ? 0 : 11 - resto
 
         // Verificar se os dígitos verificadores estão corretos
-        if (digito1 === parseInt(cpf[9]) && digito2 === parseInt(cpf[10])){
-            alert('CPF válido!');
+        if (digito1 != parseInt(cpf[9]) || digito2 != parseInt(cpf[10])){
+            alert('CPF inválido!')
+            return false
         } else {
-            alert('CPF inválido!');
+            if (tipInformado == 'J'){
+                alert('CPF não corresponde com tipo!')
+                return false
+            }
         }
-    } else if (numValidar.length == 14){
+    }
+    else if (numQuantidade == 14){
         let cnpj = numValidar
 
         // Calcular os dígitos verificadores
-        let tamanho = cnpj.length - 2;
-        let numeros = cnpj.substring(0, tamanho);
-        let digitos = cnpj.substring(tamanho);
-        let soma = 0;
-        let pos = tamanho - 7;
+        let tamanho = cnpj.length - 2
+        let numeros = cnpj.substring(0, tamanho)
+        let digitos = cnpj.substring(tamanho)
+        let soma = 0
+        let pos = tamanho - 7
 
         for (let i = tamanho; i >= 1; i--) {
-            soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
+            soma += parseInt(numeros.charAt(tamanho - i)) * pos--
             if (pos < 2) {
-                pos = 9;
+                pos = 9
             }
         }
 
-        let resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+        let resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11)
 
         // Verificar se o primeiro dígito verificador está correto
         if (resultado !== parseInt(digitos.charAt(0))) {
-            return false;
+            return false
         }
 
-        tamanho = tamanho + 1;
-        numeros = cnpj.substring(0, tamanho);
-        soma = 0;
-        pos = tamanho - 7;
+        tamanho = tamanho + 1
+        numeros = cnpj.substring(0, tamanho)
+        soma = 0
+        pos = tamanho - 7
 
         for (let i = tamanho; i >= 1; i--) {
-            soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
+            soma += parseInt(numeros.charAt(tamanho - i)) * pos--
             if (pos < 2) {
-                pos = 9;
+                pos = 9
             }
         }
 
-        resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+        resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11)
 
         // Verificar se o segundo dígito verificador está correto
-        if (resultado === parseInt(digitos.charAt(1))){
-            alert('CNPJ válido!');
+        if (resultado != parseInt(digitos.charAt(1))){
+            alert('CNPJ inválido!')
+            return false
         } else {
-            alert('CNPJ inválido!');
+            if (tipInformado == 'F'){
+                alert('CNPJ não corresponde com tipo!')
+                return false
+            }
         }
-    } else if (numValidar.length != 11 && numValidar.length != 14){
+    }
+    else if (numQuantidade != 11 || numQuantidade != 14){
         if (tipInformado == 'F'){
-            alert('CPF inválido!');
+            alert('CPF inválido!')
+            return false
         } else {
-            alert('CNPJ inválido!');
+            alert('CNPJ inválido!')
+            return false
         }
     }
 }
